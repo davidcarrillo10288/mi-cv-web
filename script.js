b377e80e902c4e54cecf99165a85749345d91966
David Carrillo Castillo <104275645+davidcarrillo10288@users.noreply.github.com>
2025-05-09 13:34:57 -0500

    cv

---
+++ script.js
@@ -0,0 +1,20 @@
+// Menú hamburguesa responsivo
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    burger.classList.toggle('toggle');
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+        nav.classList.remove('active');
+        burger.classList.remove('toggle');
+    });
+}); 
